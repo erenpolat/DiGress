10e7447cad28fda42685400f408d4140a5582493
erenpolat <63498628+erenpolat@users.noreply.github.com>
2023-11-07 21:23:31 -0800

    powerdataset setup start

--- src/datasets/powerflow_dataset.py
+++ src/datasets/powerflow_dataset.py
@@ -1,12 +1,18 @@
 import os.path as osp
-from flow_utils import graphutils
+from src.flow_utils import *
 import torch
+import pickle
 from torch_geometric.data import Dataset, download_url
+from src.datasets.abstract_dataset import AbstractDataModule, AbstractDatasetInfos
 
 
 class PowerDataset(Dataset):
-    def __init__(self, root, transform=None, pre_transform=None, pre_filter=None):
+    def __init__(self, dataset_name, split, root, transform=None, pre_transform=None, pre_filter=None):
+        self.dataset_name = "power"
+        self.split = split
+        self.num_graphs = 200
         super().__init__(root, transform, pre_transform, pre_filter)
+        self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property
     def raw_file_names(self):
@@ -17,19 +23,23 @@ class PowerDataset(Dataset):
         return ['data_1.pt', 'data_2.pt', ...]
 
     def process(self):
-        idx = 0
-        for raw_path in self.raw_paths:
-            # Read data from `raw_path`.
-            data = Data(...)
+        # Reading power data
+        data_folder = "../../data/"
+        G = read_net(data_folder + self.dataset_name + "_net.csv")
 
-            if self.pre_filter is not None and not self.pre_filter(data):
-                continue
+        pfile = open(data_folder + "features_" + self.dataset_name + ".pkl", 'rb')
+        features = pickle.load(pfile)
+        pfile.close()
 
-            if self.pre_transform is not None:
-                data = self.pre_transform(data)
+        pfile = open(data_folder + "flows_" + self.dataset_name + ".pkl", 'rb')
+        flows = pickle.load(pfile)
+        pfile.close()
 
-            torch.save(data, osp.join(self.processed_dir, f'data_{idx}.pt'))
-            idx += 1
+        #Preprocessing
+        G, flows, features = make_non_neg_norm(G, flows, features) #Converts flow estimation instance to a non-negative one, i.e. where every flow is non-negative.
+        features = normalize_features(features) #Normalize features to 0-1
+        print("erenpolat")
+        print(G.shape)
 
     def len(self):
         return len(self.processed_file_names)
@@ -37,3 +47,27 @@ class PowerDataset(Dataset):
     def get(self, idx):
         data = torch.load(osp.join(self.processed_dir, f'data_{idx}.pt'))
         return data
+class PowerGraphDataModule(AbstractDataModule):
+    def __init__(self, cfg, n_graphs=200):
+        self.cfg = cfg
+        self.datadir = cfg.dataset.datadir
+        base_path = pathlib.Path(os.path.realpath(__file__)).parents[2]
+        root_path = os.path.join(base_path, self.datadir)
+
+
+        datasets = {'train': PowerDataset(dataset_name=self.cfg.dataset.name,
+                                                 split='train', root=root_path),
+                    'val': PowerDataset(dataset_name=self.cfg.dataset.name,
+                                        split='val', root=root_path),
+                    'test': PowerDataset(dataset_name=self.cfg.dataset.name,
+                                        split='test', root=root_path)}
+        # print(f'Dataset sizes: train {train_len}, val {val_len}, test {test_len}')
+
+        super().__init__(cfg, datasets)
+        self.inner = self.train_dataset
+
+    def __getitem__(self, item):
+        return self.inner[item]
+    
+pd = PowerDataset(dataset_name="power", split="train", root="../../data")
+pd.process()
